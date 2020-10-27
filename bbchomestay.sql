93963f9480e87982a4bc5b9686d0c9d39f0b2bb9
Warlock <choochenzhung@gmail.com>
2020-10-27 19:39:27 +0800

    sql

--- bbchomestay.sql
+++ bbchomestay.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 27, 2020 at 10:21 AM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.2.33
+-- Generation Time: Oct 27, 2020 at 12:38 PM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 7.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -33,6 +34,15 @@ CREATE TABLE `alamat` (
   `poskod` int(5) NOT NULL,
   `negeri` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `alamat`
+--
+
+INSERT INTO `alamat` (`namaPelanggan`, `alamat`, `poskod`, `negeri`) VALUES
+('Choo Chen Zhung', 'Ur house', 11970, 'Penang'),
+('idTike', 'My house', 12311, 'Penang'),
+('nucleus-jumphost', 'Ur house', 11970, 'Penang');
 
 -- --------------------------------------------------------
 
@@ -61,6 +71,15 @@ CREATE TABLE `pelanggan` (
   `IDbilik` varchar(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `pelanggan`
+--
+
+INSERT INTO `pelanggan` (`IDpelanggan`, `namaPelanggan`, `No. KP`, `No. h/p`, `IDbilik`) VALUES
+('A004', 'idTike', 2147483647, 127657761, 'H1457'),
+('E3211', 'Choo Chen Zhung', 2147483647, 2147483647, 'H1456'),
+('F7001', 'Choo Chen Zhung', 2147483647, 2147483647, 'H1455');
+
 -- --------------------------------------------------------
 
 --
@@ -73,6 +92,17 @@ CREATE TABLE `pengguna` (
   `katalaluan` varchar(30) NOT NULL,
   `namaPengurus` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `pengguna`
+--
+
+INSERT INTO `pengguna` (`IDpekerja`, `namaPekerja`, `katalaluan`, `namaPengurus`) VALUES
+('D1111', 'Wh', '123', 'Cz'),
+('D2344', 'Chen', '123', 'Zhung'),
+('D6788', 'Ee', '123', 'Cheng'),
+('D6789', 'Shu', '123', 'Yi'),
+('D8900', 'Zi', '123', 'XU');
 
 -- --------------------------------------------------------
 
@@ -88,6 +118,17 @@ CREATE TABLE `tempahan` (
   `IDpekerja` varchar(5) NOT NULL,
   `bayaran` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tempahan`
+--
+
+INSERT INTO `tempahan` (`IDtempahan`, `tarikhJualan`, `tempoh`, `IDbilik`, `IDpekerja`, `bayaran`) VALUES
+('G204', '2020-10-27', 3, 'H1456', 'D1111', 600),
+('G501', '2020-10-27', 2, 'H1455', 'D1111', 200),
+('G619', '2020-10-27', 3, 'H1457', 'D1111', 1500),
+('G862', '2020-10-27', 2, 'H1455', 'D1111', 200),
+('G946', '2020-10-27', 3, 'H1455', 'D1111', 300);
 
 --
 -- Indexes for dumped tables
